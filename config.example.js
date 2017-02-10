1eed0d7ef411f6acf6ad6a460ccc78f6f96f1e63
Blacate <244787126y@gmail.com>
2017-02-10 23:30:56 +0800

    change config.example.js

--- config.example.js
+++ config.example.js
@@ -1,5 +1,10 @@
 module.exports = {
+    //server settings
     mongodb: 'mongodb://127.0.0.1:27017/dwz',
     bindAddress: '127.0.0.1',
-    bindPort: 3000
+    bindPort: 3000,
+
+    //site settings
+    siteName: "Blacate's dwz",
+    owner: "Blacate"
 }
