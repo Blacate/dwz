78ec8c89ab5855f64c18630dcd55e8fbfd5530e0
Blacate <244787126y@gmail.com>
2017-02-12 03:24:28 +0800

    changge config.example.js

--- config.example.js
+++ config.example.js
@@ -1,10 +1,15 @@
 module.exports = {
     //server settings
+    cookieSecret: 'nauj5aeSthosceasdhhu1een',
     mongodb: 'mongodb://127.0.0.1:27017/dwz',
     bindAddress: '127.0.0.1',
     bindPort: 3000,
 
     //site settings
     siteName: "Blacate's dwz",
-    owner: "Blacate"
+    owner: "Blacate",
+    loginInfo: {
+        username: "name",
+        password: "password"
+    }
 }
